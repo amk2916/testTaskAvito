4adbecfb4bfa90061548e16ebbdb22a39174bccc
Alexey Kozyrev <tcsid4@gmail.com>
2024-04-05 21:29:17 +0300

    Создал необходимые для работы сущности

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ android {
 
 dependencies {
 
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+    implementation(libs.okHttp)
+    implementation(libs.interceptor)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
