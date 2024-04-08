8e053f3d38a5e3eb4a7dd59e2cdd047a5c20f01c
Alexey Kozyrev <tcsid4@gmail.com>
2024-04-08 10:14:10 +0300

    Немного поправил макет и выдачу

--- app/src/main/java/com/example/testtaskavito/data/Mappers.kt
+++ app/src/main/java/com/example/testtaskavito/data/Mappers.kt
@@ -1,8 +1,12 @@
 package com.example.testtaskavito.data
 
 import com.example.testtaskavito.domain.MovieForList
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun Docs.toMovieForList() : MovieForList{
+    val newRating = BigDecimal(rating?.kp?.toDouble()?:0.0)
+    val finalRating = newRating.setScale(1,RoundingMode.DOWN).toString()
     return MovieForList(
         id= id,
         poster = poster?.url ?: "R.drawable.ic_launcher_foreground",
@@ -11,6 +15,6 @@ fun Docs.toMovieForList() : MovieForList{
         ageCategory = ageRating ?: 0,
 //        shortDescription = shortDescription?:"-",
         //rating = "КП:${rating?.kp ?: "-"}/IMDB:${rating?.imdb ?: "-"}"
-        rating = rating?.kp.toString()
+        rating =finalRating
     )
 }
